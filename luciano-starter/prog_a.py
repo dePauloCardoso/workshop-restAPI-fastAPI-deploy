820ecab7b2b50927d41d9c4f64d8e709c33a0262
Paulo Cardoso <cardosopaulohenrique91@gmail.com>
2025-04-21 14:05:57 -0300

    aula luciano

---
+++ luciano-starter/prog_a.py
@@ -0,0 +1,13 @@
+import random
+import time
+
+def numero_aleatorio():
+    num = random.randint(1,95)
+    print(num)
+    with open('recursos/file.txt', 'a') as file:
+        file.write(f'{num}\n')
+
+if __name__ == '__main__':
+    while True:
+        numero_aleatorio()
+        time.sleep(1)
